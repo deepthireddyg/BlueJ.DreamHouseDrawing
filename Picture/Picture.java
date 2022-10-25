48b00d423e04b2b10cf792af0a2cd6a7b0d7448e
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-10-25 07:56:54 -0400

    This is my house and changed it new look

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -16,6 +16,10 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square wall1;
+    private Square window1;
+    private Triangle roof1;
+    private Square wall2;
 
     /**
      * Constructor for objects of class Picture
@@ -30,29 +34,62 @@ public class Picture
      */
     public void draw()
     {
-        wall = new Square();
-        wall.moveVertical(80);
-        wall.changeSize(100);
-        wall.makeVisible();
-
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
-        window.makeVisible();
-
-        roof = new Triangle();
-        roof.changeSize(50, 140);
-        roof.moveHorizontal(60);
-        roof.moveVertical(70);
-        roof.makeVisible();
-
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
+        sun.moveHorizontal(100);
+        sun.moveVertical(30);
         sun.changeSize(60);
         sun.makeVisible();
+        
+        wall = new Square();
+        wall.moveVertical(80);
+
+        wall.changeSize(100);
+        wall.makeVisible();
+        
+        wall1 = new Square();
+        wall1.moveVertical(80);
+        wall1.moveHorizontal(80);
+        wall1.changeSize(100);
+        wall1.makeVisible();
+
+
+        window = new Square();
+        window.changeColor("green");
+        window.moveHorizontal(10);
+        window.moveVertical(100);
+        window.makeVisible();
+        
+        window1 = new Square();
+        window1.changeColor("green");
+        window1.moveHorizontal(120);
+        window1.moveVertical(100);
+        window1.makeVisible();
+
+
+        roof = new Triangle();
+        roof.changeSize(50, 100);
+        roof.moveHorizontal(50);
+        roof.moveVertical(70);
+        roof.makeVisible();
+        
+        roof1 = new Triangle();
+        roof1.changeSize(50, 100);
+        roof1.moveHorizontal(150);
+        roof1.moveVertical(70);
+        roof1.makeVisible();
+        
+        wall2 = new Square();
+        wall2.moveVertical(200);
+        wall2.moveHorizontal(150);
+        wall2.changeSize(20);
+        wall2.makeVisible();
+        
+        
+        
+
+
+        
     }
 
     /**
